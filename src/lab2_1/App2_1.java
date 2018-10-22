085a60636341766c26b8ee7b836e7ef365235661
D1 <botgen@gmail.com>
2018-10-23 01:49:01 +0300

    Laba 2.1 done

--- src/lab2_1/App2_1.java
+++ src/lab2_1/App2_1.java
@@ -4,15 +4,40 @@ public class App2_1 {
     public static void main(String[] args) {
         int i;
         double d;
-        boolean b;
+        char c;
+        byte by;
+        short sh;
+        long l;
+        float f;
+        boolean bool;
 
         System.out.println("Lab 2.1");
-        i = 5; //initialization
+        i = 11; //int initialization
         System.out.println(i);
-        d = 5.5; //initialization
+        d = 5.5; //double initialization
         System.out.println(d);
-        b = false; //initialization
-        System.out.println(b);
+        c = 'd'; //char initialization
+        System.out.println(c);
+        by = 8; //byte initialization
+        System.out.println(by);
+        sh = 18; //short initialization
+        System.out.println(sh);
+        l = 8; //long initialization
+        System.out.println(l);
+        f = 2.2f; //float initialization
+        System.out.println(f);
+        bool = false; //initialization
+        System.out.println(bool);
+
+        System.out.println(Integer.toHexString(i));
+        System.out.println(Double.toHexString(d));
+        System.out.println(Integer.toHexString(by));
+        //The 8-bit byte, which is signed in Java, is sign-extended to a 32-bit int
+        System.out.println(Integer.toHexString(sh));
+        //Integer is of 32 bit, and Short is of 16 bit
+        System.out.println(Long.toHexString(l));
+        System.out.println(Float.toHexString(f));
+
 
     }
 }
