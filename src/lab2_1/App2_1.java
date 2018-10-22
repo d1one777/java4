59cf2eafbf37de7586d2d2016b7e9bfba43f8fe6
D1 <botgen@gmail.com>
2018-10-22 18:24:32 +0300

    start java labs

---
+++ src/lab2_1/App2_1.java
@@ -0,0 +1,18 @@
+package lab2_1;
+
+public class App2_1 {
+    public static void main(String[] args) {
+        int i;
+        double d;
+        boolean b;
+
+        System.out.println("Lab 2.1");
+        i = 5; //initialization
+        System.out.println(i);
+        d = 5.5; //initialization
+        System.out.println(d);
+        b = false; //initialization
+        System.out.println(b);
+
+    }
+}
---
+++ src/labtestki/App.java
@@ -0,0 +1,12 @@
+package labtestki;
+
+public class App {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = " World";
+
+        System.out.println(hello == world );
+        System.out.printf(hello + world);
+
+    }
+}
