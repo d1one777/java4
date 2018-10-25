19920ec81b90a7b46d31c91499965fc3afb9a81a
D1 <botgen@gmail.com>
2018-10-26 01:26:53 +0300

    2.6 start

--- src/lab2_6/App.java
+++ src/lab2_6/App.java
@@ -1,5 +1,39 @@
 package lab2_6;
 
+import java.util.Scanner;
+
 public class App {
-    
+    final double inch = 2.54;
+    final double newtone = 9.8;
+
+    public static void main(String[] args) {
+
+        int n;
+        byte b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("+++ Welcome to MyCalc +++");
+        do
+        {
+            System.out.println("To do list:");
+            System.out.println("1. cm to inch");
+            System.out.println("2. speed in vakuum");
+            System.out.println("3. exit");
+            b = sc.nextByte();
+        }while (b < 1 || b >3);
+        switch (b) {
+            case 1    :  break;
+            case 2    :  break;
+            case 3    :  break;
+
+        }
+
+
+
+        //n = sc.nextShort();
+
+
+    }
+
+
+
 }
